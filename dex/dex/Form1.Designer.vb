f5aa7abbac18cdd29ec968e50ae2eceeaf86d556
someone <rzsto600@learner.dvusd.org>
2024-01-19 12:37:38 -0700

    literly two lines and plagerism

--- dex/dex/Form1.Designer.vb
+++ dex/dex/Form1.Designer.vb
@@ -22,11 +22,9 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label6 = New System.Windows.Forms.Label()
@@ -44,6 +42,8 @@ Partial Class Form1
         Me.b2 = New System.Windows.Forms.Button()
         Me.b3 = New System.Windows.Forms.Button()
         Me.b4 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
@@ -51,52 +51,42 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(385, 227)
+        Me.Label1.Location = New System.Drawing.Point(239, 264)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(53, 20)
+        Me.Label1.Size = New System.Drawing.Size(57, 20)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Health "
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(385, 263)
+        Me.Label2.Location = New System.Drawing.Point(239, 300)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 20)
+        Me.Label2.Size = New System.Drawing.Size(67, 20)
         Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "Defense "
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(385, 371)
+        Me.Label3.Location = New System.Drawing.Point(148, 141)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(53, 20)
+        Me.Label3.Size = New System.Drawing.Size(38, 20)
         Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Label3"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(385, 335)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(53, 20)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Label4"
+        Me.Label3.Text = "type"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(385, 299)
+        Me.Label5.Location = New System.Drawing.Point(239, 336)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(53, 20)
+        Me.Label5.Size = New System.Drawing.Size(51, 20)
         Me.Label5.TabIndex = 4
-        Me.Label5.Text = "Label5"
+        Me.Label5.Text = "Attack"
         '
         'PictureBox1
         '
         Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(264, 38)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(174, 164)
@@ -109,41 +99,41 @@ Partial Class Form1
         Me.Label6.AutoSize = True
         Me.Label6.Location = New System.Drawing.Point(148, 83)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(53, 20)
+        Me.Label6.Size = New System.Drawing.Size(46, 20)
         Me.Label6.TabIndex = 8
-        Me.Label6.Text = "Label6"
+        Me.Label6.Text = "name"
         '
         'f3
         '
-        Me.f3.Location = New System.Drawing.Point(254, 296)
+        Me.f3.Location = New System.Drawing.Point(313, 326)
         Me.f3.Name = "f3"
         Me.f3.Size = New System.Drawing.Size(125, 27)
         Me.f3.TabIndex = 9
         '
         'f5
         '
-        Me.f5.Location = New System.Drawing.Point(254, 362)
+        Me.f5.Location = New System.Drawing.Point(108, 106)
         Me.f5.Name = "f5"
         Me.f5.Size = New System.Drawing.Size(125, 27)
         Me.f5.TabIndex = 10
         '
         'f4
         '
-        Me.f4.Location = New System.Drawing.Point(254, 329)
+        Me.f4.Location = New System.Drawing.Point(108, 164)
         Me.f4.Name = "f4"
         Me.f4.Size = New System.Drawing.Size(125, 27)
         Me.f4.TabIndex = 11
         '
         'f1
         '
-        Me.f1.Location = New System.Drawing.Point(254, 230)
+        Me.f1.Location = New System.Drawing.Point(313, 260)
         Me.f1.Name = "f1"
         Me.f1.Size = New System.Drawing.Size(125, 27)
         Me.f1.TabIndex = 12
         '
         'f2
         '
-        Me.f2.Location = New System.Drawing.Point(254, 263)
+        Me.f2.Location = New System.Drawing.Point(313, 293)
         Me.f2.Name = "f2"
         Me.f2.Size = New System.Drawing.Size(125, 27)
         Me.f2.TabIndex = 13
@@ -217,11 +207,31 @@ Partial Class Form1
         Me.b4.Text = "last"
         Me.b4.UseVisualStyleBackColor = True
         '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(346, 223)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 20)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Average"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(243, 223)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(35, 20)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Stat"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.b4)
         Me.Controls.Add(Me.b3)
         Me.Controls.Add(Me.b2)
@@ -234,7 +244,6 @@ Partial Class Form1
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -252,7 +261,6 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label6 As Label
@@ -270,4 +278,6 @@ Partial Class Form1
     Friend WithEvents b2 As Button
     Friend WithEvents b3 As Button
     Friend WithEvents b4 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label7 As Label
 End Class
--- dex/dex/Form1.vb
+++ dex/dex/Form1.vb
@@ -1,16 +1,10 @@
-﻿Public Class Form1
+﻿
+
+
+Public Class Form1
     Dim records(50) As String
     Dim count As Integer
     Dim current As Integer
-    Private Sub FireToolStripMenuItem_Click(sender As Object, e As EventArgs)
-        Form2.Show()
-        Form3.Close()
-    End Sub
-
-    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles f2.TextChanged
-
-    End Sub
-
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         f1.Text = ""
         f2.Text = ""
@@ -18,76 +12,100 @@
         f4.Text = ""
         f5.Text = ""
         PictureBox1.Image = Nothing
-    End Sub
-
-    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
-        Dim outFile As New IO.StreamWriter("data.txt")
-        outFile.Write(f1.Text)
-        outFile.Write("|")
-        outFile.Write(f5.Text)
-        outFile.Write("|")
-        outFile.Write(f4.Text)
-        outFile.Write("|")
-        outFile.Write(f3.Text)
-        outFile.Write("|")
-        outFile.Write(f2.Text)
-        outFile.Write("|")
-        outFile.WriteLine(PictureBox1.Location)
-        outFile.Close()
-    End Sub
-
-    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
-        PictureBox1.Load(OpenFileDialog1.FileName)
+        current = count
+        count = count + 1
     End Sub
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         OpenFileDialog1.ShowDialog()
     End Sub
 
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        PictureBox1.Load(OpenFileDialog1.FileName)
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        SaveToFile()
+    End Sub
+    Sub SaveToFile()
+        Dim r As String
+        r += f1.Text
+        r += "|"
+        r += f2.Text
+        r += "|"
+        r += f3.Text
+        r += "|"
+        r += f4.Text
+        r += "|"
+        r += f5.Text
+        r += "|"
+        r += PictureBox1.ImageLocation
+        If count = 0 Then count = 1
+        records(current) = r
+
+        Dim outFile As New IO.StreamWriter("data.txt")
+        For index = 0 To count - 1
+            outFile.WriteLine(records(index))
+        Next
+        outFile.Close()
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If IO.File.Exists("data.txt") Then
             Dim inFile As New IO.StreamReader("data.txt")
-            While Not inFile.EndOfStream
+            While (Not inFile.EndOfStream)
                 records(count) = inFile.ReadLine
                 count = count + 1
             End While
             inFile.Close()
-            showrecord(0)
+            ShowRecord(0)
         End If
     End Sub
-    Public Sub showrecord(index As Integer)
-        Dim fields() As String
+    Public Sub ShowRecord(index As Integer)
+        PictureBox1.Image = Nothing
         If records(index) <> Nothing Then
-            fields = records(index).Split("|")
-        f1.Text = fields(0)
-        f2.Text = fields(1)
-        f3.Text = fields(2)
-        f4.Text = fields(3)
-        f5.Text = fields(4)
-            If IO.File.Exists(fields(5)) Then
-                PictureBox1.Load(fields(5))
+            Dim Fields() As String
+            Fields = records(index).Split("|")
+            f1.Text = Fields(0)
+            f2.Text = Fields(1)
+            f3.Text = Fields(2)
+            f4.Text = Fields(3)
+            f5.Text = Fields(4)
+            If IO.File.Exists(Fields(5)) Then
+                PictureBox1.Load(Fields(5))
             End If
         End If
     End Sub
 
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles b4.Click
-        current = count - 1
-        showrecord(current)
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles b3.Click
-        If current < count - 1 Then current = current + 1
-
-
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles b2.Click
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles b1.Click
+    Private Sub FirstButton_Click(sender As Object, e As EventArgs) Handles b1.Click
+        SaveToFile()
         current = 0
-        showrecord(current)
-
+        ShowRecord(current)
     End Sub
+
+    Private Sub PrevButton_Click(sender As Object, e As EventArgs) Handles b3.Click
+        SaveToFile()
+        If current > 0 Then
+            current = current - 1
+        End If
+        ShowRecord(current)
+    End Sub
+
+    Private Sub NextButton_Click(sender As Object, e As EventArgs) Handles b2.Click
+        SaveToFile()
+        If current < count - 1 Then
+            current = current + 1
+        End If
+        ShowRecord(current)
+    End Sub
+
+    Private Sub LastButton_Click(sender As Object, e As EventArgs) Handles b4.Click
+        SaveToFile()
+        If count > 0 Then
+            current = count - 1
+            ShowRecord(current)
+        End If
+    End Sub
+
+
 End Class
