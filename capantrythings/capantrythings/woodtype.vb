7a1f6222db58b555d2e69d8470b0a5fe7a3f8aa9
Royalhyena <Royalhyena8@gmail.com>
2024-03-26 13:55:56 -0700

    Minor Woodtypes changes

--- capantrythings/capantrythings/woodtype.vb
+++ capantrythings/capantrythings/woodtype.vb
@@ -3,6 +3,7 @@ Imports System.Security.Policy
 
 Public Class woodtype
 
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
         Dim f1 As New Form1
@@ -17,14 +18,20 @@ Public Class woodtype
     End Sub
 
     Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
-        TextBox2.Text = WC
+
     End Sub
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
         If ComboBox1.Text = "Pine" Then
             WC = 12.99
         End If
+        If ComboBox1.Text = "Maple" Then
+            WC = 12.99
+        End If
         If ComboBox1.Text = "Red Oak" Then
+            WC = 9.99
+        End If
+        If ComboBox1.Text = "White Oak" Then
             WC = 9.99
         End If
         If ComboBox1.Text = "Butternut" Then
@@ -33,6 +40,7 @@ Public Class woodtype
         If ComboBox1.Text = "Ash" Then
             WC = 11.99
         End If
+        TextBox2.Text = WC
     End Sub
 
     Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
@@ -42,12 +50,16 @@ Public Class woodtype
         If ComboBox2.Text = "Mahogany" Then
             WC = 17.99
         End If
-        If ComboBox2.Text = "Walnut" Or "Hard Maple" Then
+        If ComboBox2.Text = "Hard Maple" Then
+            WC = 15.99
+        End If
+        If ComboBox2.Text = "Walnut" Then
             WC = 15.99
         End If
         If ComboBox2.Text = "Teak" Then
             WC = 14.99
         End If
+        TextBox2.Text = WC
     End Sub
 
     Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
@@ -63,6 +75,6 @@ Public Class woodtype
         If ComboBox3.Text = "Agarwood" Then
             WC = 22.99
         End If
-
+        TextBox2.Text = WC
     End Sub
 End Class
